c163b58e920d233b23f042c2a7aadd7bd8a046a2
Luke <Luke@10.18.149.216>
2013-10-14 11:03:56 -0700

    luke added this

--- RPSLS/src/rpsls/Beating.java
+++ RPSLS/src/rpsls/Beating.java
@@ -21,7 +21,75 @@ public class Beating{
                 return 'L';
             }
         }
+        else if ((a == 2 && b == 4)||(a == 4 && b == 2))
+        {
+            if (a == 2)
+            {
+                System.out.println("WIN.");
+                return 'W';
+            }
+            else
+            {
+                System.out.println("LOSE.");
+                return 'L';
+            }
+        }
+        else if ((a == 3 && b == 5)||(a == 5 && b == 3))
+        {
+            if (a == 3)
+            {
+                System.out.println("WIN.");
+                return 'W';
+            }
+            else
+            {
+                System.out.println("LOSE.");
+                return 'L';
+            }
+        }
+         else if ((a == 1 && b == 4)||(a == 4 && b == 1))
+        {
+            if (a == 4)
+            {
+                System.out.println("WIN.");
+                return 'W';
+            }
+            else
+            {
+                System.out.println("LOSE.");
+                return 'L';
+            }
         
+        }
+         
+         else if ((a == 5 && b == 2)||(a == 2 && b == 5))
+        {
+            if (a == 5)
+            {
+                System.out.println("WIN.");
+                return 'W';
+            }
+            else
+            {
+                System.out.println("LOSE.");
+                return 'L';
+            }
+        }
+         
+         else if ((a == 1 && b == 5)||(a == 5 && b == 1))
+        {
+            if (a == 1)
+            {
+                System.out.println("WIN.");
+                return 'W';
+            }
+            else
+            {
+                System.out.println("LOSE.");
+                return 'L';
+            }
+        } 
+         
         else if (a > b){
             System.out.println("WIN");
             return 'W';
