6e56930c2406a4359647504fd6db2bf4246f8904
xeno1211 <hei09001@byui.edu>
2013-10-23 12:34:06 -0600

    Added validation to user name

--- RPSLS/src/rpsls/Player.java
+++ RPSLS/src/rpsls/Player.java
@@ -12,12 +12,30 @@ import java.util.Scanner;
  * @author Jeremy
  */
 public class Player {
+
     String name;
-     public void getName() {
+    boolean validName = true;
+
+    public String getName() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter your name: ");
         this.name = input.next();
+        if (this.name.matches(".*\\d.*") || this.name.matches(".*\\W.*")) {
+            System.out.println("Please enter a valid name. Names are only letters. No numbers or special characters.");
+            validName = false;
+        }
+        while (!validName) {
+            Scanner input2 = new Scanner(System.in);
+            System.out.println("Enter a VALID name: ");
+            this.name = input.next();
+            if (this.name.matches(".*\\d.*")|| this.name.matches(".*\\W.*")) {
+                System.out.println("Please enter a valid name. Names are only letters. No numbers or special characters.");
+                continue;
+            } else {
+                break;
+            }
+        }
         System.out.println("\nWelcome " + this.name + "\n");
+        return this.name;
     }
-    
 }
