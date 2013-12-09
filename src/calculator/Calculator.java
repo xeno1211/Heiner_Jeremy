1f69dda8086a1e411bb406ff973122b0e534a8ec
Luke <Luke@10.18.149.216>
2013-12-09 12:32:01 -0700

    luke added this

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author Luke
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CalculatorForm form = new CalculatorForm();
+        form.setVisible(true);
+    }
+}
